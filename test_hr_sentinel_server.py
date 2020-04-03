c1f6f961cbe64fb25357b5daa21ea043fe079232
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-03 17:45:27 -0400

    Add test case for add_patient_to_db

--- test_hr_sentinel_server.py
+++ test_hr_sentinel_server.py
@@ -1,2 +1,10 @@
 # test_hr_sentinel_server.py
 import pytest
+
+
+def test_add_patient_to_db():
+    from hr_sentinel_server import add_patient_to_db
+    answer = add_patient_to_db(3, "joeshmo@unc.edu", 53)
+    expected = {"patient_id": 3, "attending_email": "joeshmo@unc.edu",
+                "patient_age": 53}
+    assert answer == expected

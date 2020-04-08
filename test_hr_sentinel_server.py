91ac63b0ea8fb8509a4bbf2e8d107c1042682804
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-08 16:27:12 -0400

    Change add_patient unit test to add heart_rate key

--- test_hr_sentinel_server.py
+++ test_hr_sentinel_server.py
@@ -6,7 +6,7 @@ def test_add_patient_to_db():
     from hr_sentinel_server import add_patient_to_db
     answer = add_patient_to_db(3, "joeshmo@unc.edu", 53)
     expected = {"patient_id": 3, "attending_email": "joeshmo@unc.edu",
-                "patient_age": 53}
+                "patient_age": 53, "heart_rate": []}
     assert answer == expected
 
 

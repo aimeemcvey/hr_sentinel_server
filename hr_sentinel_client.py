4c292f47b3d608743db02ea2bd182953520f7200
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-11 14:23:55 -0400

    Continue testing server with client code

--- hr_sentinel_client.py
+++ hr_sentinel_client.py
@@ -5,7 +5,7 @@ server_name = "http://127.0.0.1:5000"
 
 
 def add_patients():
-    new_p = {"patient_id": 2, "attending_email": "dr@duke.edu",
+    new_p = {"patient_id": 3, "attending_email": "dr@duke.edu",
              "patient_age": 24}
     r = requests.post(server_name+"/api/new_patient", json=new_p)
     if r.status_code != 200:
@@ -22,7 +22,7 @@ def add_hr():
 
 
 def get_results():
-    r = requests.get(server_name+"/api/heart_rate/average/2")
+    r = requests.get(server_name+"/api/status/4")
     if r.status_code != 200:
         print("Error {} - {}" .format(r.status_code, r.text))
     else:

333c2af3f6d6ae00aeb53719f81e17757db63b6c
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-11 18:51:59 -0400

    Fix pep8 errors

--- hr_sentinel_client.py
+++ hr_sentinel_client.py
@@ -7,7 +7,7 @@ server_name = "http://127.0.0.1:5000"
 def add_patients():
     new_p = {"patient_id": 2, "attending_email": "dr@duke.edu",
              "patient_age": 24}
-    r = requests.post(server_name+"/api/new_patient", json=new_p)
+    r = requests.post(server_name + "/api/new_patient", json=new_p)
     if r.status_code != 200:
         print("Error: {} - {}".format(r.status_code, r.text))
     else:
@@ -16,22 +16,24 @@ def add_patients():
 
 def add_hr():
     new_hr = {"patient_id": 2, "heart_rate": 60}
-    r = requests.post(server_name+"/api/heart_rate", json=new_hr)
+    r = requests.post(server_name + "/api/heart_rate", json=new_hr)
     print(r.status_code)
     print(r.text)
 
 
 def get_results():
-    r = requests.get(server_name+"/api/status/2")
+    r = requests.get(server_name + "/api/status/2")
     if r.status_code != 200:
-        print("Error {} - {}" .format(r.status_code, r.text))
+        print("Error {} - {}".format(r.status_code, r.text))
     else:
         print("Success: {}".format(r.text))
 
 
 def get_hr_interval_avg():
-    hr_interval = {"patient_id": 2, "heart_rate_average_since": "2020-04-11 19:30:36"}
-    r = requests.post(server_name+"/api/heart_rate/interval_average", json=hr_interval)
+    hr_interval = {"patient_id": 2,
+                   "heart_rate_average_since": "2020-04-11 19:30:36"}
+    r = requests.post(server_name + "/api/heart_rate/interval_average",
+                      json=hr_interval)
     print(r.status_code)
     print(r.text)
 

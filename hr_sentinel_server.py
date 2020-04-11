f3d8e99d0e309d323a02a775b9dc44cb92ab8df0
Aimee McVey <aimee.mcvey@duke.edu>
2020-04-10 23:50:38 -0400

    Set up get framework for avg hr

--- hr_sentinel_server.py
+++ hr_sentinel_server.py
@@ -200,6 +200,20 @@ def get_all_hr(patient_id):
         return jsonify(answer), 200
 
 
+@app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
+def get_avg_hr(patient_id):
+    check_result = verify_id_input(patient_id)
+    if type(check_result) is str:
+        return check_result, 400
+    answer = generate_avg_hr(check_result)
+    if answer is False:
+        return "Unknown Error", 400
+    elif type(answer) is str:
+        return answer, 400
+    else:
+        return jsonify(answer), 200
+
+
 def verify_id_input(patient_id):
     try:
         id = int(patient_id)
